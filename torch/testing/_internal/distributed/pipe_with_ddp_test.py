31ac5779ee337646b3aec297fee06bbab174c7f9
pritam <pritam.damania@fb.com>
2020-12-15 12:11:28 -0800

    Cleanup APIs for pipeline parallelism.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/48630
    
    1) Make torch.distributed.pipeline package public.
    2) Make several helper methods private.
    ghstack-source-id: 118641381
    
    Differential Revision: [D25235688](https://our.internmc.facebook.com/intern/diff/D25235688/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D25235688/)!

--- torch/testing/_internal/distributed/pipe_with_ddp_test.py
+++ torch/testing/_internal/distributed/pipe_with_ddp_test.py
@@ -14,7 +14,10 @@ from torch.testing._internal.common_distributed import (
     skip_if_lt_x_gpu,
     skip_if_rocm,
 )
-from torch.distributed._pipeline.sync import Pipe
+from torch.testing._internal.distributed.rpc.rpc_agent_test_fixture import (
+    RpcAgentTestFixture,
+)
+from torch.distributed.pipeline.sync import Pipe
 
 class PipeWithDDPTest(RpcAgentTestFixture):
     @property
